b74cd888de149aed1d9023bcb45eb3b9a164c0cd
Lum Soon Heng <903GLWMF@psba.edu.sg>
2025-02-11 09:33:53 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,5 @@
+// Create web server
+// Create a web server that listens to incoming requests on port 3000
+// When a request comes in, read the comments.json file and respond with the contents of the file
+// If the comments.json file does not exist, respond with a 404 status code
+// If the request URL is /new, respond with the contents of the comments.json file and add the query string to the file
